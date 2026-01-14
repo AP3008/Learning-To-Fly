c4587dd3633c7946cec30574c94d2378c76e8662
Adam P <adam.porbanderwalla@gmail.com>
2026-01-13 20:12:27 -0500

    add: installed tailwindcss for this project

--- React-JS-19-Full-Course/react-app/src/App.jsx
+++ React-JS-19-Full-Course/react-app/src/App.jsx
@@ -1,30 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-const Test = ({ title }) => {
-  const [hasLiked, setHasLiked] = useState(flase);
-
-  return (
-    <div className="card">
-      <h2>{title}</h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? 'Liked':'Like'}
-      </button>
-
-    </div>
-  )
-}
 const App = () => {
   return (
-        <div className="container">
-          <Test title="This"/>
-          <Test title="is"/>
-          <Test title="a"/>
-          <Test title="Test!"/> 
-        </div>
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello World
+      </h1>
+    </div>
   )
 }
 
