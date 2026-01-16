c925cbe34f320f99bfb7337960c5f8d8ec8e4538
Adam P <adam.porbanderwalla@gmail.com>
2026-01-15 20:05:26 -0500

    feat: added search functionallity, and changed the fetch logic

--- React-JS-19-Full-Course/react-app/src/App.jsx
+++ React-JS-19-Full-Course/react-app/src/App.jsx
@@ -23,11 +23,13 @@ const App = () => {
   const [movieList, setMovieList] = useState([]);
 
   const [isLoading, setIsLoading] = useState(false); 
+  
   const fetchMovies = async (query='') => {
     setIsLoading(true); 
     setErrorMessage('');
     try{
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+      : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS); 
       if (!response.ok){
         throw new Error('failed to fetch movie'); 
@@ -53,7 +55,7 @@ const App = () => {
 
   useEffect(() => {
     fetchMovies(searchTerm); 
-  }, []);
+  }, [searchTerm]);
 
   return ( 
     <main> 
