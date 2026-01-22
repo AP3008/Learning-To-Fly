0e22e486edb569d73e5d538a434514b8a4e71185
Adam P <adam.porbanderwalla@gmail.com>
2026-01-22 11:55:38 -0500

    fix: removed vim copy and paste error

--- macos-automation-script/generic-leetcode-open.sh
+++ macos-automation-script/generic-leetcode-open.sh
@@ -5,27 +5,4 @@ open -a "$BROWSER" "https://leetcode.com/problemset/"
 open -a "$BROWSER" "https://neetcode.io/"
 open -a "$BROWSER" "YOUR LEETCODE JOURNAL / DELETE IF NOT NEEDED"
 echo "Study Environment Launched in $Browser"
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-"./leetcode-open.sh" 8L, 308B             6,121         All
+
