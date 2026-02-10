af696f27d6a214341ee7eaa9584ae98cc2a8c148
Adam P <adam.porbanderwalla@gmail.com>
2026-02-09 21:47:04 -0500

    feat: learned about rust mutability, and constants.

--- intro-rust/types.rs
+++ intro-rust/types.rs
@@ -1,4 +1,10 @@
 fn main(){
     let x = 4;
-    print!("x is: {}", x); 
+    println!("x is: {}", x); 
+    {
+        let x = 2;
+        println!("x is: {}", x);
+    }
+    let x = x + 1; 
+    println!("x is now: {}", x);
 }
