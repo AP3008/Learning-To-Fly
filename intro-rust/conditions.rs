2478b7612073d4060a059906099e5368df6807da
Adam P <adam.porbanderwalla@gmail.com>
2026-02-11 16:30:56 -0500

    feat: looked at conditions in rust

--- intro-rust/conditions.rs
+++ intro-rust/conditions.rs
@@ -1,4 +1,14 @@
 fn main() {
     let cond = (2 as f32) <=2.34;
     println!("{}", cond); 
+
+    let food = "burger"; 
+
+    if food == "cookie"{
+        println!("I like cookies");
+    } else if food == "burger" {
+        println!("Not great");
+    } else {
+        println!("You must be healthy");
+    }
 }
