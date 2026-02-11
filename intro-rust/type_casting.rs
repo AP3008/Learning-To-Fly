ff0fadc91735c9f25abbf75155fbc0b028a8e21d
Adam P <adam.porbanderwalla@gmail.com>
2026-02-11 15:56:39 -0500

    feat: learned about basic typecasting in Rust

--- intro-rust/type_casting.rs
+++ intro-rust/type_casting.rs
@@ -1,4 +1,17 @@
+use std::io; 
 fn main(){
-    let x:u8 = 9;
-    let y:i8 = 10; 
+    //let x = 127_000 as i64;
+    let y = 10 as i8;
+
+    let n = (i32::MAX as i64) + 1; 
+    let z = n + (y as i64);
+    println!("{}", z);
+
+    let int = 10 as f32; 
+    println!("{}", int+3.245);
+    
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Readline error");
+    let num_input:i64 = input.trim().parse().unwrap();
+    println!("{}",num_input + 2);
 }
