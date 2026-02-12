99d48c9eb9c25120f7e4f896c887af984a17f1e9
Adam P <adam.porbanderwalla@gmail.com>
2026-02-12 15:47:39 -0500

    feat: learned about basic ownership & borrowing with strings and &str

--- intro-rust/compound_types.rs
+++ intro-rust/compound_types.rs
@@ -6,5 +6,13 @@ fn main(){
     //Tuples
     let person:(&str, i32, bool)= ("Alice", 30, true);
     println!("{:?}", person);
+    
+    //Slices
+    let num_slice: &[i32] = &[1,2,3,4,5];
 
+    //Strings
+    let mut say: String = String::from("Hell, ");
+    println!("I say: {}", say); 
+    say.push_str("Yeah!");
+    println!("I really said: {}", say);
 }
