2041624c6b66f79eda386c43bfe35848fd3b81c5
Adam P <adam.porbanderwalla@gmail.com>
2026-02-14 22:57:14 -0500

    feat: learned about the difference between normal constant variables and actual constants using the const prefix

---
+++ intro-rust/mutability.rs
@@ -0,0 +1,13 @@
+fn main(){
+    let mut a: u8 = 5; 
+    println!("{}", a);
+    a = 16; 
+    println!("{}", a); 
+    
+    //constants
+    const Y: u8 = 10; 
+    println!("{}", Y);
+    // Y cannot be mutable because it is constant
+    println!("{}", PI);  
+}
+const PI: f64 = 3.141592653;
