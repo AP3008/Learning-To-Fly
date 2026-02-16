5561af2204916d083682b09b517d784d8ae74cd3
Adam P <adam.porbanderwalla@gmail.com>
2026-02-15 22:45:58 -0500

    chore: cleaned up file formatting and removed pub from private functions

--- rusty-file-management/src/file_organizer.rs
+++ rusty-file-management/src/file_organizer.rs
@@ -1,78 +1,85 @@
 use std::collections;
-use std::io::Error;
 use std::fs::{DirEntry, ReadDir, create_dir, read_dir, rename};
-use std::path::{Path, PathBuf}; 
+use std::io::Error;
+use std::path::{Path, PathBuf};
 
 // This function simply gets us a list of paths in a directory
-pub fn items_in_dir(path:&Path) -> Result<Vec<PathBuf>, Error>{
-    let iterator = match read_dir(path){
-        Ok(dir) => dir, 
+fn items_in_dir(path: &Path) -> Result<Vec<PathBuf>, Error> {
+    let iterator = match read_dir(path) {
+        Ok(dir) => dir,
         Err(e) => {
             println!("File path not found");
-            return Err(e); 
+            return Err(e);
         }
     };
-    
-    let mut file_list: Vec<PathBuf> = Vec::new(); 
 
-    for file in iterator{
+    let mut file_list: Vec<PathBuf> = Vec::new();
+
+    for file in iterator {
         //We need to match each of the files to DirEntry from Result
         let file = match file {
             Ok(f) => f,
-            Err(_e) => { continue; }
+            Err(_e) => {
+                continue;
+            }
         };
         file_list.push(file.path());
     }
-    return Ok(file_list);  
+    return Ok(file_list);
 }
 
 // Based of a list of paths we organize them into folders based of file extension
-pub fn group_files(paths: Vec<PathBuf>, extension: &str) -> Vec<PathBuf>{ 
-    
-    let mut grouped_list: Vec<PathBuf> = Vec::new(); 
-    
-    for path in paths{
-        match get_extension(&path){
+fn group_files(paths: Vec<PathBuf>, extension: &str) -> Vec<PathBuf> {
+    let mut grouped_list: Vec<PathBuf> = Vec::new();
+
+    for path in paths {
+        match get_extension(&path) {
             Some(ext) => {
-                if extension == ext{
+                if extension == ext {
                     grouped_list.push(path.clone());
                 }
-            }, 
-            None => { () }
+            }
+            None => (),
         };
     }
-    return grouped_list; 
+    return grouped_list;
 }
 
 // Grabbing the extension from the path
-fn get_extension(file_path: &Path)-> Option<&str>{
+fn get_extension(file_path: &Path) -> Option<&str> {
     match file_path.extension() {
-        Some(os_str) => {
-            os_str.to_str()
-        },
-        None => { None }
+        Some(os_str) => os_str.to_str(),
+        None => None,
     }
 }
 
 //Function that creates a directory based on the extension name
 //Making this specific to me so I am creating the directories inside of Random Storage on my PC
-fn create_extension_dir(extension: &str, location: &Path) -> (){
-    match create_dir(location.join(extension.to_uppercase())){
-        Ok(_d) => {println!("Created {extension} dir")},
-        Err(e) => {println!("Error: {e}")}
+fn create_extension_dir(extension: &str, location: &Path) -> () {
+    match create_dir(location.join(extension.to_uppercase())) {
+        Ok(_d) => {
+            println!("Created {extension} dir")
+        }
+        Err(e) => {
+            println!("Error: {e}")
+        }
     }
 }
 
 //Organizes files into respective folders
-fn organize(collection: Vec<PathBuf>,extension: &str, location: &Path) -> (){
-    //Check if a folder exists 
-    if !(location.exists()){
-        create_extension_dir(extension, &location); 
+fn organize(collection: Vec<PathBuf>, extension: &str, location: &Path) -> () {
+    //Check if a folder exists
+    if !(location.exists()) {
+        create_extension_dir(extension, &location);
     }
-   for file in collection{
-       match rename(&file, &location){
-        Ok(_f) => {println!("file moved!")},
-        Err(e) => {println!("Did not move: {e}")}
-       }; 
-   }
+    for file in collection {
+        match rename(&file, &location) {
+            Ok(_f) => {
+                println!("file moved!")
+            }
+            Err(e) => {
+                println!("Did not move: {e}")
+            }
+        };
+    }
 }
--- rusty-file-management/src/main.rs
+++ rusty-file-management/src/main.rs
@@ -1,22 +1,10 @@
 mod file_organizer;
-use std::{fs::create_dir, path::Path};
 
-use crate::file_organizer::{items_in_dir, group_files};
+//use crate::file_organizer::{items_in_dir, group_files};
 
 fn main() {
-    let path_str: &str = "/Users/adamporbanderwalla/Desktop";
-    let path = Path::new(path_str); 
-    let res = match items_in_dir(path){
-       Ok(res) => res,
-       Err(e) => { 
-            println!("Error reading path: {}", e);
-            return; 
-       }
-    };
-
-    //println!("{:?}",res);
-    let png = group_files(res, "png");
-    //println!("{:?}", png); 
+    //let path_str: &str = "/Users/adamporbanderwalla/Desktop";
+    //let path = Path::new(path_str); 
 }
 
 
