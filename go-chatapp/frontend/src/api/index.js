cc6b5a753a123bc36de8d3f8d56e518b50046b6e
Adam P <adam.porbanderwalla@gmail.com>
2026-03-04 21:18:04 -0500

    chore: relocated file

---
+++ go-chatapp/frontend/src/api/index.js
@@ -0,0 +1,23 @@
+var socket = new WebSocket("ws://localhost:8080/ws");
+let connect = () => {
+	console.log("Attempting to connect");	
+	socket.onopen = () => {
+		console.log("Successfully Connected");
+	};
+	socket.onmessage = msg => {
+		console.log(msg);
+	};
+	socket.onclose = event => {
+		console.log("Socket closed connection: ", event);
+	};
+	socket.onerror = error => {
+		console.log("Error occurred: ", error);
+	};
+};
+
+let sendMsg = msg => {
+	console.log("sending msg: ", msg);
+	socket.sendMsg(msg);
+};
+
+export { connect, sendMsg }
