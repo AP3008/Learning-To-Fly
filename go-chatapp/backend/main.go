7c306593071c584f8d6ce5520f831903a244e03a
Adam P <adam.porbanderwalla@gmail.com>
2026-03-04 21:01:53 -0500

    feat: defined basic websocket

--- go-chatapp/backend/main.go
+++ go-chatapp/backend/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"log"
+	"github.com/gorilla/websocket"
 )
 
 func main(){
@@ -10,6 +12,44 @@ func main(){
 	http.ListenAndServe(":8080", nil)
 }
 
+// Define an upgrader 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+	// We also need to check origin, but right now we allow any connection
+	CheckOrigin: func(r *http.Request) bool{ return true },
+}
+
+// Setup a reader that will listen for new messages being sent to websocket 
+func reader(conn *websocket.Conn){
+	for {
+		// read message 
+		messageType, p, err := conn.ReadMessage()
+		if err != nil{
+			log.Println(err)
+			return 
+		}
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil{
+			log.Println(err)
+			return 
+		}
+	}
+}
+
+// Define Websocket endpoint
+func serveWS(w http.ResponseWriter, r *http.Request){
+	fmt.Println(r.Host)
+
+	// Now upgrade the connection to websocket 
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil{
+		log.Println(err)
+		return
+	}
+	reader(ws)
+}
 func setupRoutes(){
 	http.HandleFunc("/", testServer)
 }
