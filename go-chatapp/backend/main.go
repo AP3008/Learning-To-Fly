d52033929897b1439fe24477f894f05fd62440ea
Adam P <adam.porbanderwalla@gmail.com>
2026-03-07 20:14:44 -0500

    feat: integrated websockets package

--- go-chatapp/backend/main.go
+++ go-chatapp/backend/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"go-chatapp/websocket"
 	"log"
-	"github.com/gorilla/websocket"
+	"net/http"
 )
 
 func main(){
@@ -12,43 +12,18 @@ func main(){
 	http.ListenAndServe(":8080", nil)
 }
 
-// Define an upgrader 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize: 1024,
-	WriteBufferSize: 1024,
-	// We also need to check origin, but right now we allow any connection
-	CheckOrigin: func(r *http.Request) bool{ return true },
-}
-
-// Setup a reader that will listen for new messages being sent to websocket 
-func reader(conn *websocket.Conn){
-	for {
-		// read message 
-		messageType, p, err := conn.ReadMessage()
-		if err != nil{
-			log.Println(err)
-			return 
-		}
-		fmt.Println(string(p))
-
-		if err := conn.WriteMessage(messageType, p); err != nil{
-			log.Println(err)
-			return 
-		}
-	}
-}
-
 // Define Websocket endpoint
 func serveWs(w http.ResponseWriter, r *http.Request){
 	fmt.Println(r.Host)
 
 	// Now upgrade the connection to websocket 
-	ws, err := upgrader.Upgrade(w, r, nil)
+	ws, err := websocket.Upgrade(w, r)
 	if err != nil{
 		log.Println(err)
 		return
 	}
-	reader(ws)
+	go websocket.Writer(ws)
+	websocket.Reader(ws)
 }
 
 func setupRoutes(){
