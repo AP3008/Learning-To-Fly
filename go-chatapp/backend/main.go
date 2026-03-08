e2191ffa30c64c3bade3b13f65ed6524d750e716
Adam P <adam.porbanderwalla@gmail.com>
2026-03-07 21:04:22 -0500

    feat: updated main.go w/ pool

--- go-chatapp/backend/main.go
+++ go-chatapp/backend/main.go
@@ -13,25 +13,28 @@ func main(){
 }
 
 // Define Websocket endpoint
-func serveWs(w http.ResponseWriter, r *http.Request){
-	fmt.Println(r.Host)
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request){
+	fmt.Println("Websocket Endpoint Hit")
 
 	// Now upgrade the connection to websocket 
-	ws, err := websocket.Upgrade(w, r)
+	conn, err := websocket.Upgrade(w, r)
 	if err != nil{
 		log.Println(err)
-		return
 	}
-	go websocket.Writer(ws)
-	websocket.Reader(ws)
+	client := &websocket.Client{
+		Conn: conn, 
+		Pool: pool,
+	}
+	pool.Register <- client
+	client.Read()
 }
 
 func setupRoutes(){
 	fmt.Println("Chat App v0.01")
-	http.HandleFunc("/", testServer)
-	http.HandleFunc("/ws", serveWs)
+	pool := websocket.NewPool()
+	go pool.Start()
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request){
+		serveWs(pool, w, r)
+	})
 }
 
-func testServer(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "Simple Server Setup")	
-}
--- go-chatapp/backend/pkg/websocket/websocket.go
+++ go-chatapp/backend/pkg/websocket/websocket.go
@@ -2,8 +2,6 @@
 package websocket
 
 import (
-	"fmt"
-	"io"
 	"log"
 	"net/http"
 
