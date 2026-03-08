5369725440410ddb9c40c748dea1dfaafd516b05
Adam P <adam.porbanderwalla@gmail.com>
2026-03-07 21:40:18 -0500

    chore: formatting

--- go-chatapp/backend/pkg/websocket/client.go
+++ go-chatapp/backend/pkg/websocket/client.go
@@ -3,22 +3,23 @@ package websocket
 import (
 	"fmt"
 	"log"
+
 	"github.com/gorilla/websocket"
 )
 
 type Client struct {
-	ID string
+	ID   string
 	Conn *websocket.Conn
 	Pool *Pool
 }
 
 type Message struct {
-	Type int `json:"type"`
+	Type int    `json:"type"`
 	Body string `json:"body"`
 }
 
-func (c *Client) Read(){
-	defer func () {
+func (c *Client) Read() {
+	defer func() {
 		c.Pool.Unregister <- c
 		c.Conn.Close()
 	}()
@@ -27,7 +28,7 @@ func (c *Client) Read(){
 		messageType, p, err := c.Conn.ReadMessage()
 		if err != nil {
 			log.Println(err)
-			return 
+			return
 		}
 		message := Message{Type: messageType, Body: string(p)}
 		c.Pool.Broadcast <- message
