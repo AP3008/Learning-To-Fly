19e5b2266af6354c8c42f994f09192d08d4b2497
Adam P <adam.porbanderwalla@gmail.com>
2026-03-30 20:27:38 -0400

    created: routes and init in main.go

--- url-shortner/main.go
+++ url-shortner/main.go
@@ -2,10 +2,30 @@ package main
 
 import (
 	"fmt"
+	"url-shortner/handler"
 	"url-shortner/store"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context){
+		c.JSON(200, gin.H{
+			"message": "Hey",
+		})
+	})
+
+	r.POST("/create-short-url", func(c *gin.Context){
+		handler.CreateShortUrl(c)
+	})
+
+	r.GET("/:shortUrl", func(c *gin.Context) {
+		handler.HandleShortUrlRedirect(c)
+	})
+
 	store.InitializeStore()
-	fmt.Println("URL Shortener started")
+
+	if err := r.Run(":9808"); err != nil {
+		panic(fmt.Sprintf("Failed to start the web server", err))
+	}
 }
