28d2437bc8ca4033cb9e7ebad0ff06d7d1f61365
Adam P <adam.porbanderwalla@gmail.com>
2026-04-05 15:08:02 -0400

    feat: added better frequency check using fournier transform

--- clap-start/start.py
+++ clap-start/start.py
@@ -48,21 +48,38 @@ def main():
 
             rms = np.sqrt(np.mean(audio_data.astype(np.float32)**2))
             if rms > THRESHOLD:
-                current_time = time.time()
-                time_since_last = current_time - last_clap_time
-                if clap_count == 1:
-                    if MIN_DELAY < time_since_last < MAX_DELAY:
-                        print("Opening terminal")
-                        open_terminal()
-                        clap_count = 0
-                        time.sleep(1)
-                    elif time_since_last >= MAX_DELAY:
+                fft_data = np.abs(np.fft.rfft(audio_data))
+                freqs = np.fft.rfftfreq(CHUNK, 1.0/RATE)
+
+                high_freq_energy = np.sum(fft_data[freqs > 1500])
+                total_energy = np.sum(fft_data)
+                
+                if total_energy == 0: continue 
+                
+                high_freq_ratio = high_freq_energy / total_energy
+
+                # Check if the sound is predominantly high-frequency
+                FREQ_RATIO = 0.6 
+                
+                if high_freq_ratio > FREQ_RATIO:
+                    current_time = time.time()
+                    time_since_last = current_time - last_clap_time
+
+                    if clap_count == 1:
+                        if MIN_DELAY < time_since_last < MAX_DELAY:
+                            for num in audio_data:
+                                print(num); 
+                            print("Opening terminal")
+                            open_terminal()
+                            clap_count = 0
+                            time.sleep(1)
+                        elif time_since_last >= MAX_DELAY:
+                            clap_count = 1 
+                            last_clap_time = current_time
+                    else:
                         clap_count = 1 
-                        last_clap_time = current_time
-                else:
-                    clap_count = 1 
-                    last_clap_time =current_time
-                time.sleep(0.1)
+                        last_clap_time =current_time
+                    time.sleep(0.1)
 
     except KeyboardInterrupt:
         print("Closing app")
